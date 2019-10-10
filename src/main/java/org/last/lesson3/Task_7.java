2d401c1cca00d8e774997a9cfb1d7afd886650da
Serhio-Last <serhio.last@gmail.com>
2019-10-10 21:58:41 +0300

    Lesson 3

--- src/main/java/org/last/lesson3/Task_7.java
+++ src/main/java/org/last/lesson3/Task_7.java
@@ -1,22 +1,8 @@
 package org.last.lesson3;
 
 public class Task_7 {
-    //    Write a method named getGreatestCommonDivisor with two parameters of type int named first and second.
-//    If one of the parameters is < 10, method should return -1 to indicate an invalid value.
-//    The method should return the greatest common divisor of two numbers.
-//        public static void main(String[] args) {
-//            getGreatestCommonDivisor (100, 10);
-//        }
-//        public static void getGreatestCommonDivisor(int first, int second) {
-//            for (int divisor = 1; divisor <= first && divisor <= second; divisor++) {
-//                if (first % divisor == 0 && second % divisor == 0)
-//                    if (first < 10 && second < 10) divisor = -1;
-//                System.out.println(divisor);
-//            }
-//        }
-//}
     public static void main(String[] args) {
-        getGreatestCommonDivisor(30, -18);
+        getGreatestCommonDivisor(30, 18);
     }
 
     public static int getGreatestCommonDivisor(int first, int second) {
