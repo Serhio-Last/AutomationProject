fe973b96cd64fbea08cb868754673e3c78a44928
Serhio-Last <serhio.last@gmail.com>
2019-10-10 21:56:03 +0300

    Lesson 3

--- src/main/java/org/last/lesson3/Task_7.java
+++ src/main/java/org/last/lesson3/Task_7.java
@@ -16,7 +16,7 @@ public class Task_7 {
 //        }
 //}
     public static void main(String[] args) {
-       int result = getGreatestCommonDivisor(30, -18);
+        getGreatestCommonDivisor(30, -18);
     }
 
     public static int getGreatestCommonDivisor(int first, int second) {
